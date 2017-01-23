d6de8773ba07b40c2c9c65e4374a0518d638ede4
Christopher Miller <cmill4ed@mail.umw.edu>
2017-01-23 11:54:22 -0500

    1.0.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# solid-parakeet - color model for complex numbers
+[![npm](https://img.shields.io/npm/v/solid-parakeet.svg)](https://www.npmjs.com/package/solid-parakeet)
+[![cc0](https://licensebuttons.net/p/zero/1.0/88x31.png)](https://creativecommons.org/publicdomain/zero/1.0/)
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Color_solid_comparison_hsl_hsv_rgb_cone_sphere_cube_cylinder.png/800px-Color_solid_comparison_hsl_hsv_rgb_cone_sphere_cube_cylinder.png" width=260 height=220 style="object-fit: none; object-position: -539px -380px;float:right;"/>
+
+hue is the imaginary componet. lightness the real. 100% saturation; gray is NaN.
+
+```js
+> solidparakeet( 0, 1 )
+'hsla(90, 100%, 50%, 1)'
+```
--- index.js
+++ index.js
@@ -1,3 +1,68 @@
-function z( x, y ) {
-  return Math.sqrt( 1 - ( x * x ) - ( y * y ) )
-}
+( function ( root ) {
+
+  function call( a, b ) {
+    var m = map( a, b )
+    return hsla( theta( m.z ), phi( m.x, m.y ) )
+  }
+
+  function hsla( theta, phi ) {
+    function fn( h, s, l, a ) {
+      return 'hsla('
+        + h
+        + ', '
+        + s
+        + '%, '
+        + l
+        + '%, '
+        + a
+        + ')'
+    }
+    function h( phi ) {
+      return phi * ( 180 / Math.PI )
+    }
+    var s = 100
+    function l( theta ) {
+      return ( Math.PI - theta ) / Math.PI * 100
+    }
+    var a = 1
+    if ( Number.isNaN( h( phi ) ) || Number.isNaN( l( theta ) ) ) {
+      return fn( 0, 0, 50, a)
+    }
+    return fn( h( phi ), s, l( theta ), a )
+  }
+
+  function map( a, b ) {
+    function x( a, b ) {
+      return ( 2 * a ) / ( a*a + b*b + 1 )
+    }
+    function y( a, b ) {
+      return ( 2 * b ) / ( a*a + b*b + 1 )
+    }
+    function z( c ) {
+      return ( a*a + b*b - 1 ) / ( a*a + b*b + 1 )
+    }
+    if ( Math.abs( a ) == Infinity || Math.abs( b ) == Infinity ) {
+      return { x: 0, y: 0, z: 1 }
+    }
+    return { x: x( a, b ), y: y( a, b ), z: z( a, b ) }
+  }
+
+  function phi( x, y ) {
+    return Math.atan2( y, x )
+  }
+
+  function theta( z ) {
+    return Math.acos( z )
+  }
+
+  if ( typeof define === 'function' && define.amd ) {
+    define( [], function () {
+      return call
+    } )
+  } else if ( typeof exports === 'object' ) {
+    module.exports = call
+  } else {
+    root.solidparakeet = call
+  }
+
+} )( this )
